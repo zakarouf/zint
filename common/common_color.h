91260a48d02cd4f7edd51823d5602efc87af897f
zakarouf <zak.d.official@gmail.com>
2021-02-28 01:19:17 +0530

    Added Colors

--- common/common_color.h
+++ common/common_color.h
@@ -2,10 +2,10 @@
 #define ZAKAROUF__ZINT_COMMON_COLOR_H
 
 #define color256_set(code)\
-    printf("\x1b[38;5;%d", code)
+    printf("\x1b[38;5;%dm", code)
 
 #define color256_B_set(code)\
-    printf("\x1b[48;5;%d", code)
+    printf("\x1b[48;5;%dm", code)
 
 #define color_reset() \
     printf("\x1b[0m")
