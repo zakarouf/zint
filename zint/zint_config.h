f906c6e819adcb49b39d7cd3b369331d7a349171
zakarouf <zak.d.official@gmail.com>
2021-02-28 01:22:23 +0530

    moved config defs to config

--- zint/zint_config.h
+++ zint/zint_config.h
@@ -2,6 +2,15 @@
 #define ZAKAROUF__ZINT_CONFIG_H
 
 
+ //#define ZINT_SYS__COLORS_ENABLED /* Print with colors in Loggers */
+
+ //#define ZINT_DEBUG_ENABLED     /* Comment this line out if you dont want to do a debug build */
+ //#define ZINT_DEV_TEST_ENABLED  /* Instialize Developer Test kit */
+ //#define ZINT_DEBUG__SHOW_PUSH_AND_POP_VARS_ENABLED
+ //#define ZINT_DEBUG__SHOW_PUSH_AND_POP_SCOPE_ENABLED
+
+ //#define ZINT_DEBUG__LOGGER_ENABLED
+ //#define ZINT_DEBUG__LOGGER_LOG_MEMORY
 
 
 #endif
